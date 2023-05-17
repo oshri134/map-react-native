9c34e7df485e839ca03da28c71551d0fbc48dea2
oshri shkuri <54857364+oshri134@users.noreply.github.com>
2023-05-17 13:56:29 +0300

    1

--- firebase.js
+++ firebase.js
@@ -1,20 +1,30 @@
 // Import the functions you need from the SDKs you need
+
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/database";
 import "firebase/compat/firestore";
 import { getFirestore } from "firebase/firestore";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from "@env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAnnFJExKBKdvfp_kwdKndKIwxj336GF-s",
-  authDomain: "map-react-native-b55bc.firebaseapp.com",
-  projectId: "map-react-native-b55bc",
-  storageBucket: "map-react-native-b55bc.appspot.com",
-  messagingSenderId: "809503696701",
-  appId: "1:809503696701:web:1a2a3b76833a4e0a064754",
+  apiKey: `${FIREBASE_API_KEY}`,
+  authDomain: `${FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${FIREBASE_PROJECT_ID}`,
+  storageBucket: `${FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${FIREBASE_APP_ID}`,
 };
 
 // Initialize Firebase
